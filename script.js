022836c9719d6d716f0e57c2dad7ee93e2ff0d25
VictorOmarT <victoromar001@hotmail.com>
2021-07-27 12:24:13 -0500

    Fix button place, menu-close

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const sanw = document.querySelector('.only-mob');
 const myname = document.querySelector('.name');
 
 crossbtn.addEventListener('click', () => {
+  sanw.classList.remove('Menu-mobile-after');
   menuMob.classList.remove('Menu-mobile-js');
   myname.classList.remove('Menu-mobile-after');
-  sanw.classList.remove('Menu-mobile-after');
 });
 
 sanw.addEventListener('click', () => {
+  sanw.classList.add('Menu-mobile-after');
   menuMob.classList.add('Menu-mobile-js');
   myname.classList.add('Menu-mobile-after');
-  sanw.classList.add('Menu-mobile-after');
 });
