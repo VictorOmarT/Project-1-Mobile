237b449f400b2d984726e727bcef9297c70fa40c
VictorOmarT <victoromar001@hotmail.com>
2021-07-27 11:43:48 -0500

    Fix Javascript linters

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const crossbtn = document.querySelector(".cross");
-const menuMob = document.querySelector(".Items-display");
-const sanw = document.querySelector(".only-mob");
-const myname = document.querySelector(".name");
+const crossbtn = document.querySelector('.cross');
+const menuMob = document.querySelector('.Items-display');
+const sanw = document.querySelector('.only-mob');
+const myname = document.querySelector('.name');
 
-crossbtn.addEventListener("click" , function(){
-    menuMob.classList.remove("Menu-mobile-js")
-    myname.classList.remove("Menu-mobile-after")
-    sanw.classList.remove("Menu-mobile-after")
-
+crossbtn.addEventListener('click', () => {
+  menuMob.classList.remove('Menu-mobile-js');
+  myname.classList.remove('Menu-mobile-after');
+  sanw.classList.remove('Menu-mobile-after');
 });
 
-sanw.addEventListener("click", function(){
-    menuMob.classList.add("Menu-mobile-js")
-    myname.classList.add("Menu-mobile-after")
-    sanw.classList.add("Menu-mobile-after")
+sanw.addEventListener('click', () => {
+  menuMob.classList.add('Menu-mobile-js');
+  myname.classList.add('Menu-mobile-after');
+  sanw.classList.add('Menu-mobile-after');
 });
