bf66cdf5c7e25e914428519ae7ba8bf44381ba74
VictorOmarT <victoromar001@hotmail.com>
2021-07-28 22:10:20 -0500

    Add buttons funcionality

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const crossbtn = document.querySelector('.cross');
 const menuMob = document.querySelector('.Items-display');
 const sanw = document.querySelector('.only-mob');
 const myname = document.querySelector('.name');
+const seeproy = document.querySelector('.fiveg');
+const crossmenu = document.querySelector('.multi-post-icon')
+const multipost = document.querySelector('.multi-post-box-wrapper');
+
 
 crossbtn.addEventListener('click', () => {
   sanw.classList.remove('Menu-mobile-after');
@@ -14,3 +18,12 @@ sanw.addEventListener('click', () => {
   menuMob.classList.add('Menu-mobile-js');
   myname.classList.add('Menu-mobile-after');
 });
+
+seeproy.addEventListener('click', () =>{
+  console.log("hola");
+  multipost.classList.remove('Menu-mobile-after');
+});
+
+crossmenu.addEventListener('click', () => {
+  multipost.classList.add('Menu-mobile-after');
+})
